5dfd8df05954b72882ea5c27efbe460e195296bc
Thanh Pham Kieu <thanhpk@live.com>
2018-07-29 12:33:52 +0700

    fix test

--- squasher_test.go
+++ squasher_test.go
@@ -1,44 +1,44 @@
 package squasher
 
 import (
+	"bytes"
 	"testing"
 	"time"
 )
 
 func TestSquasherLimit(t *testing.T) {
-	//t.Skip()
-	sq := NewSquasher(0, 1000)
-
-	go func() {
-		for i := range sq.Next() {
-			if i == 1000 {
-				break
-			}
-		}
-	}()
-
-	for i := 0; i < 1000; i++ {
-		sq.Mark(int64(i))
+	sq := NewSquasher(0, 7)
+	err := sq.Mark(7)
+	if err == nil {
+		t.Errorf("should be error, got nil")
 	}
 
-	err := sq.Mark(20000)
-	if err == nil {
+	sq = NewSquasher(0, 8)
+	if err = sq.Mark(14); err != nil {
+		t.Errorf("should have no error but got %v", err)
+	}
+	if err = sq.Mark(15); err == nil {
 		t.Errorf("should be error, got nil")
 	}
 }
 
+
 func TestFirstZeroBit(t *testing.T) {
-	//t.Skip()
 	cs := []struct {
-		x   byte
-		out uint
+		x     byte
+		start uint
+		out   uint
 	}{
-		{0xFF, 8},
-		{0xF1, 1}, // 1111 0001
+		{0xFF, 0, 0}, // 1111 1111
+		{0xF1, 0, 1}, // 1111 0001
+		{0x06, 1, 3}, // 0000 0110
+		{0x06, 2, 3}, // 0000 0110
+		{0x06, 3, 3}, // 0000 0110
+		{0xE7, 5, 3}, // 1110 0111
 	}
 
 	for _, c := range cs {
-		out := getFirstZeroBit(c.x)
+		out := getFirstZeroBit(c.x, c.start)
 		if out != c.out {
 			t.Errorf("with byte %x, expect %d, got %d", c.x, c.out, out)
 		}
@@ -46,7 +46,6 @@ func TestFirstZeroBit(t *testing.T) {
 }
 
 func TestNextMissingIndex(t *testing.T) {
-	//t.Skip()
 	cs := []struct {
 		circle            []byte
 		value             int64
@@ -58,6 +57,7 @@ func TestNextMissingIndex(t *testing.T) {
 		{[]byte{0xFF, 0x1F, 0}, 2100, 0, 2, 2110, 1, 4}, // 1111 1111 | 0001 1111 | --
 		{[]byte{0, 0xFF, 0x05}, 2100, 1, 4, 2104, 2, 0}, // -- | 1111 1111 | - 0101
 		{[]byte{0, 0xFF, 0}, 2100, 1, 4, 2103, 1, 7},    // -- | 1111 1111 | -
+		{[]byte{0, 0xF0, 0}, 2100, 1, 4, 2103, 1, 7},    // -- | 1111 1111 | -
 		{[]byte{0x0F, 0, 0xFF}, 2100, 2, 0, 2111, 0, 3}, // - 1111 | - | 1111 1111
 		{[]byte{0xff, 0xff, 0xff, 0x3f, 0}, 0, 0, 0, 29, 3, 5},
 		{[]byte{0xff, 0, 0xff, 0xff}, 0, 2, 0, 23, 0, 7},
@@ -82,22 +82,59 @@ func TestNextMissingIndex(t *testing.T) {
 }
 
 func TestNextNonFFByte(t *testing.T) {
-	//t.Skip()
 	ts := []struct {
-		circle             []byte
-		start_byte, output uint
+		circle                        []byte
+		start_byte, start_bit, output uint
 	}{
-		{[]byte{0xFF, 0xF4, 0}, 0, 1},
-		{[]byte{0xFF, 0xFF, 0xF4, 0xFF, 0xFE, 0}, 3, 4},
-		{[]byte{0xFE, 0xFF, 0}, 0, 0},
-		{[]byte{0xFF, 0xFF, 0}, 0, 2},
-		{[]byte{0, 0xFF, 0xFF}, 1, 0},
+		{[]byte{0xFF, 0xF4, 0}, 0, 0, 1},
+		{[]byte{0xFF, 0xFF, 0xF4, 0xFF, 0xFE, 0}, 3, 0, 4},
+		{[]byte{0xFE, 0xFF, 0}, 0, 0, 0},
+		{[]byte{0xFF, 0xFF, 0}, 0, 0, 2},
+		{[]byte{0, 0xFF, 0xFF}, 1, 0, 0},
+		{[]byte{0, 0xF0, 0xFF}, 1, 4, 0},
+		{[]byte{0, 0xF0, 0xFF}, 1, 0, 1},
 	}
 	for _, c := range ts {
-		out := getNextNonFFByte(c.circle, c.start_byte)
+		out := getNextNonFFByte(c.circle, c.start_byte, c.start_bit)
 		if out != c.output {
 			t.Errorf("with circle %v and start_byte %d, expect %d, got %d", c.circle, c.start_byte, c.output, out)
 		}
+	}
+}
+
+func TestSquasherCleanBit(t *testing.T) {
+	sq := NewSquasher(0, 15)
+
+	errc := make(chan int64)
+	go func() {
+		for i := range sq.Next() {
+			if i > 30 {
+				errc <- i
+				break
+			}
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		sq.Mark(int64(i))
+	}
+
+	for i := 6; i < 18; i++ {
+		if err := sq.Mark(int64(i)); err != nil {
+			panic(err)
+		}
+	}
+	if 0 != bytes.Compare(sq.circle, []byte{0xA7, 0xFF}) {
+		t.Errorf("should be equal")
+	}
+
+	sq.Mark(18)
+	sq.Mark(19)
+
+	sq.Mark(5)
+
+	if 0 != bytes.Compare(sq.circle, []byte{0x10, 0x00}) {
+		t.Errorf("should be equal")
 	}
 }
 
@@ -114,14 +151,15 @@ func TestSquasherClean(t *testing.T) {
 		}
 	}()
 
-
 	for i := 1; i < 23; i++ {
 		sq.Mark(int64(i))
 	}
 
 	sq.Mark(0)
-	sq.Mark(23); sq.Mark(24); sq.Mark(25); sq.Mark(26)
-	sq.Mark(27); sq.Mark(28); sq.Mark(29); sq.Mark(30)
+	for i := 23; i < 31; i++ {
+		sq.Mark(int64(23))
+	}
+
 	select {
 	case i := <-errc:
 		t.Errorf("got 30 before commit: %d", i)
@@ -130,7 +168,6 @@ func TestSquasherClean(t *testing.T) {
 }
 
 func TestSquasher(t *testing.T) {
-	//t.Skip()
 	sq := NewSquasher(0, 4)
 
 	gotc := make(chan int64)
@@ -140,7 +177,6 @@ func TestSquasher(t *testing.T) {
 			gotc <- i
 		}
 	}()
-
 
 	sq.Mark(2)
 	sq.Mark(3)
@@ -162,7 +198,6 @@ func TestSquasher(t *testing.T) {
 }
 
 func TestSquasher100(t *testing.T) {
-	//t.Skip()
 	sq := NewSquasher(0, 500)
 
 	gotc := make(chan int64)
@@ -190,12 +225,12 @@ func TestSquasher100(t *testing.T) {
 }
 
 func TestSquasherTurnAround(t *testing.T) {
-	//t.Skip()
 	sq := NewSquasher(0, 100)
 
 	donec := make(chan bool, 0)
 	go func() {
 		for i := range sq.Next() {
+
 			if i == 1000 {
 				break
 			}
@@ -211,12 +246,11 @@ func TestSquasherTurnAround(t *testing.T) {
 }
 
 func TestSetBit(t *testing.T) {
-	//t.Skip()
 	ts := []struct {
-		circle             []byte
+		circle                []byte
 		start_byte, start_bit uint
-		start_value, val int64
-		out_circle []byte
+		start_value, val      int64
+		out_circle            []byte
 	}{
 		{[]byte{0, 0xF5, 0xD1, 0xF6}, 1, 0, 10, 20, []byte{0, 0xF5, 0xD5, 0xF6}},
 		{[]byte{0, 0x80, 0xD5, 0xB4}, 1, 7, 10, 20, []byte{0, 0x80, 0xD5, 0xB6}},
