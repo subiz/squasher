b87607065764b6f69f231871acd16c1732e8f06c
Thanh Pham Kieu <thanhpk@live.com>
2020-02-08 23:24:01 +0700

    add function to expand circle and change the next api

--- squasher.go
+++ squasher.go
@@ -7,12 +7,13 @@ import (
 
 type Squasher struct {
 	*sync.Mutex
-	circle      []byte
+	circle []byte
+
 	start_value int64
 	start_byte  uint
 	start_bit   uint
-	nextchan    chan int64 // next chan receive next value
-	latest      int64
+
+	latest int64
 }
 
 func (s *Squasher) Print() {
@@ -24,60 +25,75 @@ func (s *Squasher) Print() {
 }
 
 // a circle with size = 2
-//     start_byte: 1 --v     v-- start_bit: 4
-// byt 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 3 3 3 3 3 3 3 3
-// bit 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0
-// cir 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0 0 1 1 0 1 1 1 1 1 0 1 0 0
+//     start_byte: 1 ---v     v-- start_bit: 4
+// byt 0 0 0 0 0 0 0 0  1 1 1 1 1 1 1 1  2 2 2 2 2 2 2 2  3 3 3 3 3 3 3 3
+// bit 7 6 5 4 3 2 1 0  7 6 5 4 3 2 1 0  7 6 5 4 3 2 1 0  7 6 5 4 3 2 1 0
+// cir 0 0 0 0 0 0 0 0  0 0 0 1 0 0 1 0  1 0 0 0 1 1 0 1  1 1 1 1 0 1 0 0
 //
-//
-func NewSquasher(start int64, size int32) *Squasher {
-	if size < 2 {
-		size = 2
-	}
-	size++
-	circlelen := size / 8 // number of byte to store <size> bit
-	if size%8 > 0 {
-		circlelen++
-	}
-
-	circle := make([]byte, circlelen, circlelen)
+func NewSquasher(start int64) *Squasher {
+	circle := make([]byte, 32, 32)
 	circle[0] = 1
 	s := &Squasher{
 		Mutex:       &sync.Mutex{},
-		nextchan:    make(chan int64, 1),
 		start_value: start - 1,
+		start_byte:  0,
+		start_bit:   0,
 		circle:      circle,
 	}
 	return s
 }
 
-// closeCircle set the end_byte (byte before start_byte) to zero
+// zeroCircle zero all bits from byte to byte
 func zeroCircle(circle []byte, frombyt, tobyt, frombit, tobit uint) {
 	ln := uint(len(circle))
-	if ln <= frombyt || ln <= tobyt {
-		panic("something wrong here, from or to is greater than len of circle")
+	if ln == 0 {
+		return
 	}
+
 	// zero out first byte
 	var mask byte
-	if frombyt == tobyt {
+
+	// handle the first end the last byte
+	if frombyt == tobyt || ln == 1 {
+		if frombit > 7 {
+			return // invalid
+		}
+
+		if tobit > 7 {
+			tobit = 7
+		}
+
+		if frombit == tobit {
+			return // do nothing
+		}
+
+		if frombit < tobit {
+			for i := frombit; i < tobit; i++ {
+				mask |= 1 << (i % 8)
+			}
+			mask ^= 0xFF
+			circle[frombyt] &= mask
+			return
+		}
+
 		for i := frombit; i%8 != tobit; i++ {
 			mask |= 1 << (i % 8)
 		}
 		mask ^= 0xFF
 		circle[frombyt] &= mask
-		if frombit < tobit {
-			return
-		}
-		frombyt++
-	} else {
-		var mask byte = (0xFF >> tobit) << tobit
-		circle[tobyt] &= mask
 
-		mask = (0xFF << (8 - frombit) >> (8 - frombit))
-		circle[frombyt] &= mask
+		// set other byte to zero
+		for i := uint(0); i < ln; i++ {
+			if i != frombyt {
+				circle[i] = 0
+			}
+		}
+		return
 	}
 
-	for i := frombyt; i%ln != tobyt; i++ {
+	circle[frombyt] = circle[frombyt] << (8 - frombit) >> (8 - frombit)
+	circle[tobyt] = circle[tobyt] >> tobit << tobit
+	for i := frombyt + 1; i%ln != tobyt; i++ {
 		circle[i%ln] = 0
 	}
 }
@@ -95,8 +111,35 @@ func setBit(circle []byte, start_byte, start_bit uint, start_value, i int64) {
 	circle[nextbyte] |= 1 << bit
 }
 
+// return new cirle, keep the start bit
+func expandCircle(circle []byte, start_byte, start_bit, newsize uint) []byte {
+	newlen := uint(len(circle)) // in byte
+	if newlen*8 > newsize+1 {
+		// still enough room, don't alloc new size
+		return circle
+	}
+
+	for true {
+		newlen = newlen * 2
+		if newlen*8 > newsize+1 {
+			break
+		}
+	}
+	newcircle := make([]byte, newlen, newlen)
+
+	oldlen := uint(len(circle))
+	for i := uint(0); i < oldlen; i++ {
+		newcircle[i] = circle[(start_byte+i)%oldlen]
+	}
+
+	// close the circle
+	newcircle[oldlen] = circle[start_byte] << (8 - start_bit) >> (8 - start_bit)
+	newcircle[0] = circle[start_byte] >> start_bit << start_bit
+	return newcircle
+}
+
 // Mark a value i as processed
-func (s *Squasher) Mark(i int64) error {
+func (s *Squasher) Mark(i int64) int64 {
 	s.Lock()
 	defer s.Unlock()
 	if i > s.latest {
@@ -104,35 +147,25 @@ func (s *Squasher) Mark(i int64) error {
 	}
 	dist := i - s.start_value
 	if dist <= 0 {
-		return nil
+		return s.start_value
 	}
 	ln := uint(len(s.circle))
-
-	if uint(dist) > (ln)*8-1 {
-		return fmt.Errorf("out of range, i should be less than %d", int64(ln)*8+s.start_value)
+	if uint(dist)+1 > ln*8 {
+		s.circle = expandCircle(s.circle, s.start_byte, s.start_bit, uint(dist))
+		s.start_byte = 0
 	}
 
 	setBit(s.circle, s.start_byte, s.start_bit, s.start_value, i)
+
 	if dist != 1 {
-		return nil
+		return s.start_value
 	}
 	nextval, nextbyte, nextbit :=
-		getNextMissingIndex(s.circle, s.start_value, s.start_byte, s.start_bit)
+		getNextStart(s.circle, s.start_value, s.start_byte, s.start_bit)
+
 	zeroCircle(s.circle, s.start_byte, nextbyte, s.start_bit, nextbit)
 	s.start_value, s.start_byte, s.start_bit = nextval, nextbyte, nextbit
-	s.pushNext(nextval)
-	return nil
-}
-
-// not thread safe, so remember to lock before call this
-func (s *Squasher) pushNext(val int64) {
-	// flush out next channel first
-	select {
-	case <-s.nextchan:
-	default:
-	}
-
-	s.nextchan <- val
+	return s.start_value
 }
 
 // getFirstZeroBit return the first zero bit
@@ -165,9 +198,8 @@ func getNextNonFFByte(circle []byte, start_byte, start_bit uint) uint {
 	return i % ln
 }
 
-func getNextMissingIndex(circle []byte, start_value int64, start_byte, start_bit uint) (int64, uint, uint) {
+func getNextStart(circle []byte, start_value int64, start_byte, start_bit uint) (int64, uint, uint) {
 	ln := uint(len(circle))
-
 	byt := getNextNonFFByte(circle, start_byte, start_bit)
 	sbit := start_bit
 	if byt != start_byte {
@@ -192,9 +224,7 @@ func getNextMissingIndex(circle []byte, start_value int64, start_byte, start_bit
 	return start_value + int64(dist), byt % ln, bit
 }
 
-func (s *Squasher) Next() <-chan int64 { return s.nextchan }
-
 func (s *Squasher) GetStatus() string {
-	ofs, _, _ := getNextMissingIndex(s.circle, s.start_value, s.start_byte, s.start_bit)
+	ofs, _, _ := getNextStart(s.circle, s.start_value, s.start_byte, s.start_bit)
 	return fmt.Sprintf("[%d .. %d .. %d]", s.start_value, ofs, s.latest)
 }
